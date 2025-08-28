5978564059cd395f14466734cb3ae56778910470
Pham Tran Minh Duc <ducprodapchai243@gmail.com>
2025-08-28 22:02:55 +0700

    Initial Commit

--- about.js
+++ about.js
@@ -4,20 +4,18 @@ import { dirname, join } from "path";
 
 const app = express();
 
-// Fix for __dirname in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Serve static files from the build directory
+// Serve static files
 app.use(express.static(join(__dirname, "build")));
 
-// Serve index.html on root
-app.get("/", (req, res) => {
+// Catch-all: send index.html for React Router paths
+app.get("*", (req, res) => {
   res.sendFile(join(__dirname, "build", "index.html"));
 });
 
-// Start server
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
